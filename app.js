00e367b34e9636608bca9875de47fc06c9ee2296
SyedaIshrat <Rasyedais@gmail.com>
2018-04-16 18:19:03 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -2,14 +2,25 @@
 var host = "api.giphy.com";
 var path = "/v1/gifs/random";
 var apiKey = "WzJFIWuFzLiEZ87B8f8Af2OH844AF5Dj"; 
-var tag = "sleepy"; 
+var tag = "sleepy";
+var tag2 = "fire"; 
 var api = "https://" + host + path + "?api_key=" + apiKey + "&tag=" + tag;
+var bkg = "https://" + host + path + "?api_key=" + apiKey + "&tag=" + tag2;
+
+// var audio = new Audio('audio_file.mp3');
+
 
 // shows random GIF
 function showGif() {
     $.getJSON(api, function(giphy) {
-        gifUrl = giphy.data.image_original_url;
-        $('#img-container').prepend('<img src="' + gifUrl + '">');
+        $.getJSON(bkg, function(bkgGiphy) {
+            gifUrl = giphy.data.image_original_url;
+
+            $('#img-container').prepend('<img src="' + gifUrl + '">');
+            document.body.style.backgroundImage = 'url(' + gifUrl + ')';
+        })
+        
+        
     });
 }
 
